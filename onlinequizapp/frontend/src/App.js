129becb6b5d8dbfdee6a846f600507e0124379d1
Savuoye <sony.ankit@outlook.com>
2025-07-29 08:27:23 +0530

    Commiting Frontent UI Page

---
+++ onlinequizapp/frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
