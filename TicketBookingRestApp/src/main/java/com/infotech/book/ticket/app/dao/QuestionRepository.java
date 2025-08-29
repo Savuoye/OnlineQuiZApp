c9470bf63576f481ba14d0ab340452cb86329481
Savuoye <sony.ankit@outlook.com>
2025-08-29 18:00:43 +0000

    Adding question functionality

---
+++ TicketBookingRestApp/src/main/java/com/infotech/book/ticket/app/dao/QuestionRepository.java
@@ -0,0 +1,10 @@
+package com.infotech.book.ticket.app.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.infotech.book.ticket.app.entities.Questions;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Questions, Long> {
+}
