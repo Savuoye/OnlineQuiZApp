6687de80d06ea0cf86d1a18bf421e2a21074d4d0
Savuoye <sony.ankit@outlook.com>
2025-08-29 18:42:54 +0000

    Updating and deleting questions

--- TicketBookingRestApp/src/main/java/com/infotech/book/ticket/app/dao/QuestionRepository.java
+++ TicketBookingRestApp/src/main/java/com/infotech/book/ticket/app/dao/QuestionRepository.java
@@ -1,5 +1,7 @@
 package com.infotech.book.ticket.app.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import com.infotech.book.ticket.app.entities.Questions;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Questions, Long> {
+	
+	Questions findById(long id); 
+	 Optional<Questions> findByQuestionId(Long quizId);
 }
